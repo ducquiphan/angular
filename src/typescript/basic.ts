83bd7890e293f8d20b5ef6acfce3268d6cc6d492
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-19 18:00:56 +0700

    Typescript Base Types and Primitives

---
+++ src/typescript/basic.ts
@@ -0,0 +1,18 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+age = 12;
+let userName: string;
+userName = 'James';
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+let result = add(3, 5);
+
+let isInstructor: boolean = false;
