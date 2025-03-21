72d582692944f3df9980de2e19b32edb71441d52
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-21 15:01:25 +0700

    Create Custom Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {HeaderComponent} from './pages/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular18-course-pj';
--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
+  //standalone: true, // If using Angular 19 or higher, this is set to true by default
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
 
