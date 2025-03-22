5b84b49d6d8f476649a93f7c2a643e52c0eca7b8
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-22 14:09:13 +0700

    Create Custom Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {HeaderComponent} from './pages/header/header.component';
+import {UserComponent} from './pages/user/user.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    imports: [HeaderComponent, UserComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular18-course-pj';
+    title = 'angular18-course-pj';
 }
