c1b31296225da366ac0473f073be8514c786372b
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-22 15:38:35 +0700

    Inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import {Component} from '@angular/core';
+import {DUMMY_USERS} from './dummy-users';
 import {HeaderComponent} from './pages/header/header.component';
 import {UserComponent} from './pages/user/user.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [HeaderComponent, UserComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
+	selector: 'app-root',
+	imports: [HeaderComponent, UserComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css',
 })
 export class AppComponent {
-    title = 'angular18-course-pj';
+	title = 'angular18-course-pj';
+	users = DUMMY_USERS;
+
+
 }
--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, signal} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {DUMMY_USERS} from '../../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,16 +10,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 	styleUrl: './user.component.css',
 })
 export class UserComponent {
-	selectedUser = signal(DUMMY_USERS[randomIndex]);
-	imagePath = computed(() => 'users/' + this.selectedUser().avatar);
+	@Input() avatar!: string;
+	@Input() name!: string;
 
-	// get imagePath() {
-	// 	return 'users/' + this.selectedUser().avatar;
-	// }
+	get imagePath() {
+		return 'users/' + this.avatar;
+	}
 
 	onSelectUser() {
-		const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-		this.selectedUser.set(DUMMY_USERS[randomIndex]);
-		console.log('Clicked!');
+
 	}
 }
