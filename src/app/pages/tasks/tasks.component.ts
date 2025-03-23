625b45411ae84c1b63e805564664b3ce0e1d93e6
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-23 16:56:00 +0700

    44. Adding More Components to the Demo App

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -1,8 +1,11 @@
 import {Component, input} from '@angular/core';
+import {TaskComponent} from './task/task.component';
 
 @Component({
 	selector: 'app-tasks',
-	imports: [],
+	imports: [
+		TaskComponent,
+	],
 	templateUrl: './tasks.component.html',
 	styleUrl: './tasks.component.css',
 })
