6ab0710602e6e00b98c26bed8c17870e3d7dd892
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-23 15:43:55 +0700

    Create a Configurable Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {DUMMY_USERS} from './dummy-users';
 import {HeaderComponent} from './pages/header/header.component';
+import {TasksComponent} from './pages/tasks/tasks.component';
 import {UserComponent} from './pages/user/user.component';
 
 @Component({
 	selector: 'app-root',
-	imports: [HeaderComponent, UserComponent],
+	imports: [HeaderComponent, UserComponent, TasksComponent],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
 })
 export class AppComponent {
 	title = 'angular18-course-pj';
 	users = DUMMY_USERS;
+	selectedUser = signal(DUMMY_USERS[0]);
 
 	onSelectUser(id: string) {
 		console.log('Selected use with id: ' + id);
+		this.selectedUser.set(this.users.find(user => user.id===id)!);
 	}
+
+
 }
