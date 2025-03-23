203e7606a9b189fc5dbcc374a2630d65efeab99c
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-23 14:04:56 +0700

    Signal Inputs

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, computed, input} from '@angular/core';
 import {DUMMY_USERS} from '../../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,12 +10,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 	styleUrl: './user.component.css',
 })
 export class UserComponent {
-	@Input({required: true}) avatar!: string;
-	@Input({required: true}) name!: string;
+	// @Input({required: true}) avatar!: string;
+	// @Input({required: true}) name!: string;
+	avatar = input.required<string>(); // Telling Angular that this is required. Required value cannot be set an initial value
+	name = input<string>('');
+	imagePath = computed(() => {
+		return 'users/' + this.avatar();
+	});
 
-	get imagePath() {
-		return 'users/' + this.avatar;
-	}
+
+	// get imagePath() {
+	// 	return 'users/' + this.avatar;
+	// }
 
 	onSelectUser() {
 
