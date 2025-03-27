0cbf22b030c460b046a44222170efffdc6b76b43
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-28 01:04:27 +0700

    110. Scoping CSS Styles to Components

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,12 +1,10 @@
 import {Component} from '@angular/core';
-import {NzIconDirective} from 'ng-zorro-antd/icon';
 import {ButtonComponent} from '../shared/button/button.component';
 
 @Component({
 	selector: 'app-header',
 	imports: [
 		ButtonComponent,
-		NzIconDirective,
 	],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.css',
