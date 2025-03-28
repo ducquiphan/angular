49519cd16aaaacd129a4ebb9b4ad7b986efb3094
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-28 21:53:47 +0700

    114. Interacting With Host Elements From Inside Components

--- src/app/pages/shared/control/control.component.ts
+++ src/app/pages/shared/control/control.component.ts
@@ -14,6 +14,9 @@ import {NzColDirective, NzRowDirective} from 'ng-zorro-antd/grid';
 	templateUrl: './control.component.html',
 	styleUrl: './control.component.css',
 	encapsulation: ViewEncapsulation.None,
+	host: {
+		class: 'control',
+	},
 })
 export class ControlComponent {
 	label = input.required<{ for: string, name: string }>();
