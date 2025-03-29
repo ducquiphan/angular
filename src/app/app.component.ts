d8f0d8e0d27235a5b5780fd9f8873594c4c04784
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-30 01:11:54 +0700

    125. Component Cleanup with ngOnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,21 @@
 import {Component} from '@angular/core';
-import {DashboardItemComponent} from './pages/dashboard/dashboard-item/dashboard-item.component';
-import {ServerComponent} from './pages/dashboard/server/server.component';
-import {SupportComponent} from './pages/dashboard/support/support.component';
-import {TrafficComponent} from './pages/dashboard/traffic/traffic.component';
-import {HeaderComponent} from './pages/header/header.component';
+
+import {LifecycleComponent} from './lifecycle/lifecycle.component';
 
 @Component({
 	selector: 'app-root',
-	standalone: true,
 	templateUrl: './app.component.html',
-	imports: [
-		SupportComponent,
-		HeaderComponent,
-		ServerComponent,
-		TrafficComponent,
-		DashboardItemComponent,
-	],
+	imports: [LifecycleComponent],
 })
 export class AppComponent {
-	
+	lifecycleComponentIsVisible = false;
+	lifecycleInputText = 'Some Random Number: ' + Math.random() * 100;
+
+	onToggleLifecycleComponentVisibility() {
+		this.lifecycleComponentIsVisible = !this.lifecycleComponentIsVisible;
+	}
+
+	onChangeLifecycleInputText() {
+		this.lifecycleInputText = 'Some Random Number: ' + Math.random() * 100;
+	}
 }
