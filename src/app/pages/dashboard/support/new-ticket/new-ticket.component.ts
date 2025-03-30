004579690b02739b4614a5bb8b3baafb25dccf89
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-30 14:44:16 +0700

    129. Extracting Input Values via Template Variables

--- src/app/pages/dashboard/support/new-ticket/new-ticket.component.ts
+++ src/app/pages/dashboard/support/new-ticket/new-ticket.component.ts
@@ -22,9 +22,9 @@ import {ControlComponent} from '../../../shared/control/control.component';
 })
 export class NewTicketComponent {
 
-	onSubmit(titleEl: HTMLInputElement, requestEl: HTMLTextAreaElement) {
-		const enteredTitle = titleEl.value;
-		const enteredRequest = requestEl.value;
+	onSubmit(title: string, request: string) {
 		console.log('Submitted!');
+		console.log(title);
+		console.log(request);
 	}
 }
