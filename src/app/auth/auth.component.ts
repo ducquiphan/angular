14bf2a47c89dd5923a8eea5993ac9565a882276f
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-04-04 18:51:23 +0700

    160. Host Directives & Composition

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,21 +1,23 @@
-import { Component, inject, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {Component, inject, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {LogDirective} from '../directives/log.directive';
 
-import { AuthService } from './auth.service';
+import {AuthService} from './auth.service';
 
 @Component({
-  selector: 'app-auth',
-  standalone: true,
-  imports: [FormsModule],
-  templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css',
+	selector: 'app-auth',
+	standalone: true,
+	imports: [FormsModule],
+	templateUrl: './auth.component.html',
+	styleUrl: './auth.component.css',
+	hostDirectives: [LogDirective],
 })
 export class AuthComponent {
-  email = signal('');
-  password = signal('');
-  private authService = inject(AuthService);
+	email = signal('');
+	password = signal('');
+	private authService = inject(AuthService);
 
-  onSubmit() {
-    this.authService.authenticate(this.email(), this.password());
-  }
+	onSubmit() {
+		this.authService.authenticate(this.email(), this.password());
+	}
 }
