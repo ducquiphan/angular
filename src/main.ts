50ae5e25a5b14b810778984758b554da77eced41
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2026-01-21 00:48:37 +0700

    183. There Are Multiple Ways Of Providing a Service

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import {bootstrapApplication} from '@angular/platform-browser';
 
-import { AppComponent } from './app/app.component';
+import {AppComponent} from './app/app.component';
+import {TasksService} from './app/tasks/tasks.service';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [TasksService] // bad practice as this always initial instance no mater of usage
+}).catch((err) => console.error(err));
