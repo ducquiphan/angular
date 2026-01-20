4b5e943f63cfc01c57527c8717b112458fb49f32
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2026-01-20 23:56:21 +0700

    180. Using The Alternative Dependency Injection Syntax

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,14 +1,21 @@
-import {Injectable, signal} from '@angular/core';
+import {Injectable, Signal, signal, WritableSignal} from '@angular/core';
 import {Task} from './task.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
-
-  tasks = signal<Task[]>([]);
-
   constructor() {
+  }
+
+  private _tasks = signal<Task[]>([]);
+
+  get tasks(): Signal<Task[]> {
+    return this._tasks.asReadonly();
+  }
+
+  set tasks(value: WritableSignal<Task[]>) {
+    this._tasks = value;
   }
 
   addTask(taskData: { title: string, description: string }) {
@@ -17,7 +24,7 @@ export class TasksService {
       id: Math.random().toString(36),
       status: 'OPEN'
     }
-    this.tasks.update((oldTasks) =>
+    this._tasks.update((oldTasks) =>
       [...oldTasks, newTask]
     );
   }
